9103e5559bb528d0c0e466c07a6e156dc479a4f3
Ricardo Lafuente <r@manufacturaindependente.org>
2017-11-11 17:00:11 +0000

    Preparar para pôr live

--- post-empresa.py
+++ post-empresa.py
@@ -4,17 +4,18 @@
 import random
 from utils import post_to_mastodon, post_to_twitter, download_file, get_csv_column_values
 
-POST_TO_MASTODON = True
-POST_TO_TWITTER = False
+POST_TO_MASTODON = False
+POST_TO_TWITTER = True
 
+# sacar o CSV
 filename = download_file('https://framacalc.org/eCbVG6EF2x.csv', 'empresas.csv')
-
+# extrair os valores de cada coluna
 slogans = get_csv_column_values(filename, 'slogan')
 names = get_csv_column_values(filename, 'nome')
-
+# escolher um de cada ao calhas
 slogan = random.choice(slogans)
 name = random.choice(names)
-
+# criar o slogan substituindo o placeholder pelo nome da empresa
 post_content = slogan.replace('xxxxx', name)
 
 print
