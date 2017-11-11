8f7fc77d8ee3a16b2dbf8d9e15663dc9502f87c7
Ricardo Lafuente <r@manufacturaindependente.org>
2017-11-11 17:00:36 +0000

    mv post-empresa.py post.py

--- post.py
+++ post.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from utils import post_to_mastodon, post_to_twitter, download_file
+import random
+from utils import post_to_mastodon, post_to_twitter, download_file, get_csv_column_values
 
 POST_TO_MASTODON = False
-POST_TO_TWITTER = False
+POST_TO_TWITTER = True
 
-post_content = "Hello world!"
+# sacar o CSV
+filename = download_file('https://framacalc.org/eCbVG6EF2x.csv', 'empresas.csv')
+# extrair os valores de cada coluna
+slogans = get_csv_column_values(filename, 'slogan')
+names = get_csv_column_values(filename, 'nome')
+# escolher um de cada ao calhas
+slogan = random.choice(slogans)
+name = random.choice(names)
+# criar o slogan substituindo o placeholder pelo nome da empresa
+post_content = slogan.replace('xxxxx', name)
 
-print(post_content)
+print
+print('  ' + post_content)
+print
 
 if POST_TO_MASTODON:
     post_to_mastodon(post_content)
